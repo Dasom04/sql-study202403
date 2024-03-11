95aa36b8f2777f80ca762a58cb6cf677dd95696c
dasom <slz_952@naver.com>
2024-03-11 17:25:55 +0900

    데이터타입 함수, 형변환, 집합연산자

---
+++ 05_숫자날짜함수.sql
@@ -0,0 +1,71 @@
+
+-- 숫자함수
+-- ROUND(반올림)
+-- 원하는 반올림 위치를 매개값로 지정, 음수를 주는것도 가능.
+-- (-1)음수는 소수점을 넘어서 1에 자리에서 반올림을 한다.
+SELECT
+    ROUND(3.1415, 3), ROUND(45.923, 0), ROUND(45.923, -1)
+FROM dual;
+
+-- TRUNC(절사)
+-- 정해진 소수점 자리수까지 잘라냅니다.
+-- (-1)음수는 소수점을 넘어서 1에 자리를 없애버린다
+SELECT
+    TRUNC(3.1415, 3), TRUNC(45.923, 0), TRUNC(45.923, -1)
+FROM dual;
+
+-- ABS (절대값)
+SELECT ABS(-34)FROM dual;
+
+-- CEIL(올림), FLOOR(내림)
+SELECT CEIL(3.14), FLOOR(3.14)
+FROM dual;
+
+-- MOD(나머지 나눗셈 연산)
+SELECT
+    10 / 4, MOD(10, 4)
+FROM dual;
+
+-----------------------------------------------------------------------------------
+
+-- 날짜 함수
+-- sysdate: 컴퓨터의 현재 날짜, 시간 정보를 가져와서 제공하는 함수 -> 시간 정보도 있음!
+SELECT sysdate FROM dual;
+SELECT systimestamp FROM dual; -- -> 밀리초, 표준시까지 제공
+
+-- 날짜도 연산이 가능합니다. 숫자를 더하거나 빼면 day가 늘거나 줄어든다.
+SELECT sysdate + 1 FROM dual; -- 일자 덧셈 연산이 가능.
+
+-- 날짜 타입과 날짜 타입은 뺄셈 연산을 지원합니다.
+-- 덧셈은 허용하지 않습니다.
+SELECT
+    first_name, sysdate - hire_date
+FROM employees; -- 일자
+
+SELECT
+    first_name,
+    (sysdate - hire_date) / 7 AS week
+FROM employees; -- 주수
+
+SELECT
+    first_name,
+    (sysdate - hire_date) / 365 AS year
+FROM employees; -- 년수
+
+-- 날짜 반올림, 절사
+SELECT ROUND(sysdate) FROM dual;
+SELECT ROUND(sysdate, 'year') FROM dual;
+SELECT ROUND(sysdate, 'month') FROM dual;
+SELECT ROUND(sysdate, 'day') FROM dual;
+
+
+
+
+
+
+
+
+
+
+
+
